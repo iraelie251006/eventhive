8c1921eb307061b5d39a07e2afafa32e5d3da5cd
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-27 23:31:27 +0200

    Initialized EventHive project

---
+++ src/main/java/dev/iraelie/eventhive/EventhiveApplication.java
@@ -0,0 +1,13 @@
+package dev.iraelie.eventhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/iraelie/eventhive/EventhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iraelie.eventhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
