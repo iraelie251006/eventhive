0dc5617f703ac04fe904ac1597ba483eac68df79
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-05 20:41:16 +0200

    added entity fields for the Venue

--- src/main/java/dev/iraelie/eventhive/model/Venue.java
+++ src/main/java/dev/iraelie/eventhive/model/Venue.java
@@ -2,7 +2,14 @@ package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.Entity;
 
+import java.util.Set;
+
 @Entity
 public class Venue {
     private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private Set<Hall> halls;
 }
