ed97abd3df7856ab8d9d5cf3da4c0397fc8219e6
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-06 23:37:50 +0200

    Added Setter lombok on Venue class

--- src/main/java/dev/iraelie/eventhive/model/Venue.java
+++ src/main/java/dev/iraelie/eventhive/model/Venue.java
@@ -2,10 +2,12 @@ package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Getter
+@Setter
 @Entity
 public class Venue {
     private Long id;
