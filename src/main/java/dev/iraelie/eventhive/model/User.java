de9a0791b22277dd720c163f112d09004ef797b2
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-07 23:38:26 +0200

    Added Builder lombok annotation on User class

--- src/main/java/dev/iraelie/eventhive/model/User.java
+++ src/main/java/dev/iraelie/eventhive/model/User.java
@@ -1,11 +1,13 @@
 package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
