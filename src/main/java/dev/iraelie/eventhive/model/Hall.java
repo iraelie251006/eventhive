9f951306383a1e9e2e45bc2349b4b9d6a8727736
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-07 23:36:36 +0200

    Added Builder lombok annotation on Hall class

--- src/main/java/dev/iraelie/eventhive/model/Hall.java
+++ src/main/java/dev/iraelie/eventhive/model/Hall.java
@@ -1,11 +1,13 @@
 package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.Entity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @Entity
 public class Hall {
     private Long id;
