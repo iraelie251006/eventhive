8200dd42f3a5b025ee4bd6d17531571e7afe41f1
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-08 22:30:08 +0200

    annotated id to generated value annotation

--- src/main/java/dev/iraelie/eventhive/model/Ticket.java
+++ src/main/java/dev/iraelie/eventhive/model/Ticket.java
@@ -1,6 +1,7 @@
 package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Builder;
 
@@ -8,5 +9,6 @@ import lombok.Builder;
 @Entity
 public class Ticket {
     @Id
+    @GeneratedValue
     private Long id;
 }
