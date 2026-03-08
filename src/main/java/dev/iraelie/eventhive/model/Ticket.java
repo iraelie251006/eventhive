2a8f6fd3286ea210ece79fd79101dbe130a874a1
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-08 22:28:28 +0200

    annotated ticket id with @Id annotation

--- src/main/java/dev/iraelie/eventhive/model/Ticket.java
+++ src/main/java/dev/iraelie/eventhive/model/Ticket.java
@@ -1,10 +1,12 @@
 package dev.iraelie.eventhive.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Builder;
 
 @Builder
 @Entity
 public class Ticket {
+    @Id
     private Long id;
 }
