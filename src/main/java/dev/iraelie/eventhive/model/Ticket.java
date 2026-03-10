e4aedbf1569b76555fcb3c7727955534512a2865
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-10 23:42:05 +0200

    simplified imports

--- src/main/java/dev/iraelie/eventhive/model/Ticket.java
+++ src/main/java/dev/iraelie/eventhive/model/Ticket.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
