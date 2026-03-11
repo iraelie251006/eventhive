0d42763f3aef82d94a347992de7af52f617403c4
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-11 23:37:28 +0200

    Added RequiredArgsConstructor annotation

--- src/main/java/dev/iraelie/eventhive/model/Hall.java
+++ src/main/java/dev/iraelie/eventhive/model/Hall.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 @Builder
 @Entity
 public class Hall {
