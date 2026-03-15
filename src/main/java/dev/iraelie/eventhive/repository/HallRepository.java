b1c2112ef5523059d83dcb30ed7ff7f7193753d4
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-15 23:54:51 +0200

    extended JpaRepository for HallRepository

--- src/main/java/dev/iraelie/eventhive/repository/HallRepository.java
+++ src/main/java/dev/iraelie/eventhive/repository/HallRepository.java
@@ -1,7 +1,9 @@
 package dev.iraelie.eventhive.repository;
 
+import dev.iraelie.eventhive.model.Hall;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HallRepository {
+public interface HallRepository extends JpaRepository<Hall, Long> {
 }
