e2125de2db7f3c58358580cfe93791263658fdaf
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-15 23:54:10 +0200

    extended JpaRepository for TicketRepository

--- src/main/java/dev/iraelie/eventhive/repository/TicketRepository.java
+++ src/main/java/dev/iraelie/eventhive/repository/TicketRepository.java
@@ -1,7 +1,9 @@
 package dev.iraelie.eventhive.repository;
 
+import dev.iraelie.eventhive.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
