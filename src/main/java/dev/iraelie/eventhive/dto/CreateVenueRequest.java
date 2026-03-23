c2a115c140854becc09a2102adbd1234d32bc9ab
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-23 23:09:20 +0200

    implemented CreateVenueRequest dto class

--- src/main/java/dev/iraelie/eventhive/dto/CreateVenueRequest.java
+++ src/main/java/dev/iraelie/eventhive/dto/CreateVenueRequest.java
@@ -1,4 +1,17 @@
 package dev.iraelie.eventhive.dto;
 
+import lombok.*;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Builder
 public class CreateVenueRequest {
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private Set<HallRequest> halls;
 }
