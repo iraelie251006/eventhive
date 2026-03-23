46f963acd861641d441c0935c979a32207390e46
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-23 23:16:26 +0200

    created VenueRepository file for better data retrieval

--- src/main/java/dev/iraelie/eventhive/repository/VenueRepository.java
+++ src/main/java/dev/iraelie/eventhive/repository/VenueRepository.java
@@ -1,7 +1,10 @@
 package dev.iraelie.eventhive.repository;
 
+import dev.iraelie.eventhive.model.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VenueRepository {
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+    boolean findByName(String name);
 }
