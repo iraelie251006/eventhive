4fcb5e184bdcf7c14bd32b5127256f81b4cb7604
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-23 23:19:45 +0200

    Implemented Venue service

--- src/main/java/dev/iraelie/eventhive/service/VenueServiceImpl.java
+++ src/main/java/dev/iraelie/eventhive/service/VenueServiceImpl.java
@@ -1,7 +1,36 @@
 package dev.iraelie.eventhive.service;
 
+import dev.iraelie.eventhive.dto.CreateVenueRequest;
+import dev.iraelie.eventhive.model.Venue;
+import dev.iraelie.eventhive.repository.VenueRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
-public class VenueServiceImpl {
+@RequiredArgsConstructor
+@Slf4j
+public class VenueServiceImpl implements VenueService {
+    private final VenueRepository venueRepository;
+
+    @Override
+    public void createVenue(CreateVenueRequest request) {
+        if (venueRepository.findByName(request.getName())) {
+            throw new RuntimeException("Venue already exist");
+        }
+
+        Venue venue = Venue.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .city(request.getCity())
+                .country(request.getCountry())
+                .halls(Set.of((dev.iraelie.eventhive.model.Hall) request.getHalls()))
+                .build();
+
+        Venue savedVenue = venueRepository.save(venue);
+
+        return;
+    }
 }
