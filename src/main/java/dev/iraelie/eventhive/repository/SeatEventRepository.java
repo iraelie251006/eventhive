3304266f3ca56cf80886f4c83c2e056759750a37
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-24 23:55:24 +0200

    extended JpaRepository in SeatEventRepository

--- src/main/java/dev/iraelie/eventhive/repository/SeatEventRepository.java
+++ src/main/java/dev/iraelie/eventhive/repository/SeatEventRepository.java
@@ -1,7 +1,9 @@
 package dev.iraelie.eventhive.repository;
 
+import dev.iraelie.eventhive.model.EventSeat;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SeatEventRepository {
+public interface SeatEventRepository extends JpaRepository<EventSeat, Long> {
 }
