6331bd04ec3ada06899744f4c3617e88929bfed8
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-01 23:53:45 +0200

    enabled JPA auditing

--- src/main/java/dev/iraelie/eventhive/EventhiveApplication.java
+++ src/main/java/dev/iraelie/eventhive/EventhiveApplication.java
@@ -2,8 +2,10 @@ package dev.iraelie.eventhive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EventhiveApplication {
 
 	public static void main(String[] args) {
